1c4c96bd04f31dcc712a062b6469ccb1ab27e145
Nieman <nieman123@gmail.com>
2024-04-28 19:56:36 -0400

    adding evaluate

---
+++ src/evaluate.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import accuracy_score, classification_report
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions.round())
+    report = classification_report(y_test, predictions.round())
+    logging.info(f"Model accuracy: {accuracy}")
+    logging.info(f"Classification Report: \n{report}")
+    return accuracy, report
