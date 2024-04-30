dcf8a0a23070fb16e48c14b8bded20487cf01ff8
Nieman <nieman123@gmail.com>
2024-04-30 14:41:03 -0400

    full feature perdiction

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,18 +1,26 @@
 import numpy as np 
 
+def create_features(notes, total_positions=8):  # Example max_duration, adjust based on data exploration
+    """ Create a feature matrix from notes data. """
+    pitches = normalize_pitch(notes[:, 2])
+    velocities = normalize_velocity(notes[:, 3])
+    normalized_starts = normalize_quantized_starts(notes[:, 0], total_positions)
+    durations = normalize_durations(notes[:, 0], notes[:, 1], max_duration=500)
+    return np.stack([normalized_starts, durations, pitches, velocities], axis=1)
+
 def normalize_pitch(pitches):
     """ Normalize MIDI pitches to a 0-1 range based on MIDI standards (0-127). """
     return pitches / 127.0
 
-def encode_durations(starts, ends):
-    """ Convert start and end times to durations and normalize. """
+def normalize_velocity(velocities):
+    """ Normalize MIDI velocities to a 0-1 range based on MIDI standards (0-127). """
+    return velocities / 127.0
+
+def normalize_durations(starts, ends, max_duration):
+    """ Normalize durations by a predetermined maximum duration to ensure consistency across the dataset. """
     durations = ends - starts
-    max_duration = durations.max()
     return durations / max_duration
 
-def create_features(notes):
-    """ Create a feature matrix from notes data. """
-    pitches = normalize_pitch(notes[:, 2])
-    velocities = notes[:, 3] / 127.0  # Normalize velocities in the same way as pitches
-    durations = encode_durations(notes[:, 0], notes[:, 1])
-    return np.stack([pitches, velocities, durations], axis=1)
+def normalize_quantized_starts(starts, total_positions):
+    """ Normalize quantized start times to a range between 0 and 1. """
+    return starts / (total_positions - 1) 
