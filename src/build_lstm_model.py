cafe3b7221aa290220db5f5a40cb53bf15f8d561
Nieman <nieman123@gmail.com>
2024-05-02 10:50:27 -0400

    Model update

--- src/build_lstm_model.py
+++ src/build_lstm_model.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout  
 
-def build_lstm_model(input_shape, num_units=64, dropout_rate=0.3, output_units=206):
+def build_lstm_model(input_shape, num_units=64, dropout_rate=0.3):
     model = Sequential([
         LSTM(num_units, input_shape=input_shape, return_sequences=True),
         Dropout(dropout_rate),
         LSTM(num_units, return_sequences=True),
         Dropout(dropout_rate),
         Dense(num_units, activation='relu'),
-        Dense(4, activation='sigmoid')  # Assume binary classification for simplicity
+        Dense(4, activation='sigmoid')  # Outputting 4 features: start, duration, pitch, velocity
     ])
     
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
     return model
