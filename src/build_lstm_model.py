39daca7c636704914c656f91928034e00edd2d92
Nieman <nieman123@gmail.com>
2024-05-07 19:51:21 -0400

    ltsm model improvements

--- src/build_lstm_model.py
+++ src/build_lstm_model.py
@@ -1,15 +1,30 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout  
+from keras.layers import LSTM, Dense, Dropout, Bidirectional, BatchNormalization
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler
+import math
 
 def build_lstm_model(input_shape, num_units=64, dropout_rate=0.3):
     model = Sequential([
-        LSTM(num_units, input_shape=(1, input_shape[1]), return_sequences=True),
+        Bidirectional(LSTM(num_units, return_sequences=True, input_shape=(1, input_shape[1]))),
+        BatchNormalization(),
         Dropout(dropout_rate),
-        LSTM(num_units, return_sequences=True),
+        Bidirectional(LSTM(num_units, return_sequences=True)),
+        BatchNormalization(),
         Dropout(dropout_rate),
         Dense(num_units, activation='relu'),
         Dense(64, activation='sigmoid')  # Outputting 4 features: start, duration, pitch, velocity
     ])
     
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_squared_error'])
+    optimizer = Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mean_squared_error'])
     return model
+
+def step_decay(epoch):
+    initial_lrate = 0.001
+    drop = 0.5
+    epochs_drop = 10.0
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    return lrate
+
+lrate = LearningRateScheduler(step_decay)
--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,6 @@
 from build_lstm_model import build_lstm_model
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, LearningRateScheduler
+import math
 
 def train_model(X_train, y_train, X_val, y_val, input_shape):
     # Reshape the data to add the time_steps dimension (1 in this case)
@@ -10,9 +11,20 @@ def train_model(X_train, y_train, X_val, y_val, input_shape):
 
     input_shape = (1, X_train.shape[2])  # (time_steps, features)
     
+    # Early Stopping
     early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    
+    # Learning Rate Scheduler
+    def step_decay(epoch):
+        initial_lrate = 0.001
+        drop = 0.5
+        epochs_drop = 10.0
+        lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+        return lrate
+
+    lrate = LearningRateScheduler(step_decay)
 
     model = build_lstm_model(input_shape)
-    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping])
+    model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping, lrate])
     model.save("crappy_midi_gen.keras")
     return model
